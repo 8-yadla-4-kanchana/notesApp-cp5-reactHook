8002e42f41de851310e7e386fc8d91741c0c649c
8-yadla-4-kanchana <kanchanashyam1998@gmail.com>
2023-05-15 06:16:33 +0000

    done

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,15 @@
+// Write your code here
+import {NoteCard, NoteHeading, NoteText} from './styledComponents'
+
+const NoteItem = props => {
+  const {each} = props
+  const {title, description} = each
+  return (
+    <NoteCard>
+      <NoteHeading>{title}</NoteHeading>
+      <NoteText>{description}</NoteText>
+    </NoteCard>
+  )
+}
+
+export default NoteItem
